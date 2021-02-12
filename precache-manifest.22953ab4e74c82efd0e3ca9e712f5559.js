edc6fe3296545e7ba646d60cdfa5c1e5dafe27e7
Rashmi <imhsarv@gmail.com>
2021-02-12 21:29:58 +0530

    Updates

---
+++ precache-manifest.22953ab4e74c82efd0e3ca9e712f5559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faa23bbef489d49ce5feb06bb299556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77354e667355b09b5c91",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d932be857e7ecc28aa81",
+    "url": "/static/js/2.0bdd13ea.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.0bdd13ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77354e667355b09b5c91",
+    "url": "/static/js/main.0883e572.chunk.js"
+  },
+  {
+    "revision": "bf9fda22f05c083606a6",
+    "url": "/static/js/runtime-main.ccf115f7.js"
+  }
+]);
